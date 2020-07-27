8e6b9d5a85a728678d59110ce63595eed4246183
전준엽 <galid1@naver.com>
2020-07-27 10:07:45 +0900

    mod 토큰 생성및 발급을 위한 설정 추가, h2console 설정
    - 토큰 생성시 username, password 인증을 위한 AuthenticationManager 추가
    - h2 console 사용을 위한 설정 추가

--- src/main/java/com/galid/study/config/SpringSecurityConfig.java
+++ src/main/java/com/galid/study/config/SpringSecurityConfig.java
@@ -1,30 +1,40 @@
 package com.galid.study.config;
 
 import com.galid.study.security.RestAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+import java.util.Arrays;
 
 @Configuration
+@RequiredArgsConstructor
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .cors().disable()
             .csrf().disable()
             .authorizeRequests()
-                .antMatchers("/signUp", "/signIn")
+                .antMatchers("/signUp/**", "/signIn/**", "/h2-console/**")
                 .permitAll()
-                .antMatchers("/t1")
-                .hasAuthority("USER")
-                .antMatchers("/t2")
-                .hasAuthority("USER")
-            .anyRequest().authenticated()
+                .anyRequest().authenticated()
+                .and()
+            .headers().frameOptions().disable()
                 .and()
             .exceptionHandling()
                 .authenticationEntryPoint(restAuthenticationEntryPoint())
@@ -45,4 +55,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         return new RestAuthenticationEntryPoint();
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+    }
 }
